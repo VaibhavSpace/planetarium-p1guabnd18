9ddcbbcbae2fa6e2797247ab054dab0f9b305e1d
exospecies <exospecies@gmail.com>
2018-06-09 19:07:34 +0530

    Initial commit

---
+++ app/src/main/java/project1/abnd2018winner/android/planetarium/MainActivity.java
@@ -0,0 +1,16 @@
+package project1.abnd2018winner.android.planetarium;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.SpannableString;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
